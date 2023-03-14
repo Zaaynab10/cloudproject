7e0e286bff267f26a0e52e8697dd86e15f307544
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2023-03-14 12:36:35 +0000

    cloud project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function App() {
+
+ 
+  return (
+    <div className="App">
+ <h1 style={{color:"#ca6702", fontFamily:"initial",textAlign:"center", marginTop:"55px",fontSize:"123px"}}> Our Location ... </h1>
+ <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1273.654249769028!2d-73.96616482423121!3d40.782813116770825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2589a018531e3%3A0xb9df1f7387a94119!2sCentral%20Park!5e0!3m2!1sfr!2ssn!4v1678795796725!5m2!1sfr!2ssn" width="600" height="450" style={{border:"0",position:"absolute",top:"220px",left:"423px"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  );
+}
+
+export default App;
